6a8f9e460a963148a8fb1cee39c1e794cea9db62
dvthnight <davide.lanotte@protonmail.com>
2022-04-12 18:01:11 +0200

    creato classe utente non registrato con istanze

--- utente_non_registrato.php
+++ utente_non_registrato.php
@@ -1,1 +1,19 @@
 <?php
+class Utente_non_registrato{
+    public $nome;
+    public $cognome;
+    public $metodo_pagamento;
+
+    function __construct($_nome,$_cognome,$_metodo_pagamento)
+    {
+        $this->nome = $_nome;
+        $this->cognome = $_cognome;
+        $this->metodo_pagamento = $_metodo_pagamento;
+    }
+}
+
+$un1 = new Utente_non_registrato("Paolo","Panda","carta");
+$un2 = new Utente_non_registrato("Rosario","Fiorello","carta");
+$un = new Utente_non_registrato("Simone","Pretelli","carta");
+
+var_dump($un1);
