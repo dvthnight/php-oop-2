0d592bc84a429fe566cc7d0a7bc7b5113296fb46
dvthnight <davide.lanotte@protonmail.com>
2022-04-17 10:53:50 +0200

    importato tutte le classi in index

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
 <?php
+require_once __DIR__."/carta_di_credito.php";
+require_once __DIR__."/carrello.php";
+require_once __DIR__."/ordine.php";
+require_once __DIR__."/prodotto.php";
+require_once __DIR__."/utente_non_registrato.php";
+require_once __DIR__."/utente_registrato.php";
+
+
+
